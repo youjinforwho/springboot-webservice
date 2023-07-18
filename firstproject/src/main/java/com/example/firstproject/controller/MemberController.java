218efa00821689d21dcd4139eef6382893581c32
youjinforwho <ox8764@naver.com>
2023-07-18 11:49:44 +0900

    회원 관리 기능

---
+++ firstproject/src/main/java/com/example/firstproject/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MemberController {
+    //회원가입 페이지 출력 요청
+    @GetMapping("/member/save")
+    public String saveForm(){
+        return "save";
+    }
+}
